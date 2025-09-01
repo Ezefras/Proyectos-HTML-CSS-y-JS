07c4df942feb20b5b2ea99cb3604c90123f0ce1d
Ezequiel Fraschini <luisroltoni@gmail.com>
2025-09-01 14:56:16 -0300

    Actualice la pagina y el js del simulador de dados

--- Roll Dice Simulator/javascript.js
+++ Roll Dice Simulator/javascript.js
@@ -1,14 +1,22 @@
-const dice1 = new Image();
-dice1.src = "images/imagenes dados.png";
-const dice2 = new Image();
-dice2.src = "images/imagenes dados (2).png";
-const dice3 = new Image();
-dice3.src = "images/imagenes dados (3).png";
-const dice4 = new Image();
-dice4.src = "images/imagenes dados (4).png";
-const dice5 = new Image();
-dice5.src = "images/imagenes dados (5).png";
-const dice6 = new Image();
-dice6.src = "images/imagenes dados (6).png";
+const button1 = document.getElementById("button1")
+const diceImages = [
+    "images/imagenes dados.png",
+    "images/imagenes dados (2).png",
+    "images/imagenes dados (3).png",
+    "images/imagenes dados (4).png",
+    "images/imagenes dados (5).png",
+    "images/imagenes dados (6).png"
+];
 
-const diceImg = document.getElementById("diceImgs")
+const diceImg = document.getElementById("diceImg")
+function diceChanger() {
+    for (let i = 20; i > 0; i--) {
+        setTimeout(() => {
+            const randomIndex = Math.floor(Math.random() * diceImages.length);
+            diceImg.src = diceImages[randomIndex];
+        }, (10 - i) * 150); // 300ms entre cada cambio
+    }
+}
+
+// Listener afuera de la función
+button1.addEventListener("click", diceChanger);
